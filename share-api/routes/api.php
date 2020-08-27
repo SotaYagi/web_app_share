d38f9138b18253d8366eef3d678b975d1770e099
“Sota” <dec.sota12@gmail.com>
2020-08-27 16:53:34 +0900

    message

---
+++ share-api/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::apiResource('/shares', 'ShareController');
+Route::post('/register', 'RegisterController@post');
+Route::post('/login', 'LoginController@post');
+Route::post('/logout', 'LogoutController@post');
+Route::get('/user', 'UsersController@get');
+Route::put('/user', 'UsersController@put');
+Route::post('/like', 'LikesController@post');
+Route::delete('/like', 'LikesController@delete');
+Route::post('/comment', 'CommentsController@post');
